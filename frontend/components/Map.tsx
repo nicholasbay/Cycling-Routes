6fdd17db8cb2e712336de5f6f29f3923d822f623
nicholasbay <bay.yong.wei@gmail.com>
2026-01-13 13:50:13 +0800

    Add MAX_ZOOM and MIN_ZOOM constants for map configuration

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -4,7 +4,7 @@ import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
 
 import { MapContent } from './MapContent';
-import { DEFAULT_CENTER, DEFAULT_ZOOM} from '@/constants';
+import { DEFAULT_CENTER, DEFAULT_ZOOM, MAX_ZOOM, MIN_ZOOM } from '@/constants';
 
 export default function Map(props: any) {
   const { userPosition } = props
@@ -19,8 +19,8 @@ export default function Map(props: any) {
     >
       <ZoomControl position='bottomright' />
       <TileLayer
-        maxZoom={19}
-        minZoom={11}
+        maxZoom={MAX_ZOOM}
+        minZoom={MIN_ZOOM}
         attribution='<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
         url='https://www.onemap.gov.sg/maps/tiles/Default_HD/{z}/{x}/{y}.png'
       />
--- frontend/constants.ts
+++ frontend/constants.ts
@@ -1,2 +1,4 @@
 export const DEFAULT_CENTER: [number, number] = [1.2868, 103.8545];
 export const DEFAULT_ZOOM: number = 15;
+export const MAX_ZOOM: number = 19;
+export const MIN_ZOOM: number = 11;
