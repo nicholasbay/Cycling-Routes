65a590e412a3501088fee25c941179fe52507893
nicholasbay <bay.yong.wei@gmail.com>
2026-01-13 13:38:38 +0800

    Move zoom control to bottom right and set min. and max. zoom restrictions

--- frontend/components/Map.tsx
+++ frontend/components/Map.tsx
@@ -1,4 +1,4 @@
-import { MapContainer, TileLayer } from 'react-leaflet';
+import { MapContainer, TileLayer, ZoomControl } from 'react-leaflet';
 import 'leaflet/dist/leaflet.css';
 import 'leaflet-defaulticon-compatibility';
 import 'leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css';
@@ -11,12 +11,16 @@ export default function Map(props: any) {
 
   return (
     <MapContainer
+      className='h-full w-full'
       center={DEFAULT_CENTER}
       zoom={DEFAULT_ZOOM}
       scrollWheelZoom={true}
-      style={{ height: '100%', width: '100%' }}
+      zoomControl={false}
     >
+      <ZoomControl position='bottomright' />
       <TileLayer
+        maxZoom={19}
+        minZoom={11}
         attribution='<a href="https://www.onemap.gov.sg/" target="_blank" rel="noopener noreferrer">OneMap</a>&nbsp;&copy;&nbsp;contributors&nbsp;&#124;&nbsp;<a href="https://www.sla.gov.sg/" target="_blank" rel="noopener noreferrer">Singapore Land Authority</a>'
         url='https://www.onemap.gov.sg/maps/tiles/Default_HD/{z}/{x}/{y}.png'
       />
@@ -24,4 +28,3 @@ export default function Map(props: any) {
     </MapContainer>
   );
 }
-
